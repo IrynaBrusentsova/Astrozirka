d3f4ce9eff6f619554f8ac62fd51dccafc63592e
Iryna Brusentsova <bio200480@gmail.com>
2024-07-04 14:35:30 +0300

    Fix function scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,61 +1,57 @@
-'use strict';
-const body = document.querySelector('body');
+"use strict";
+const body = document.querySelector("body");
 console.log(body);
 
-const burger = document.querySelector('.navbar-toggler-icon');
-console.log (burger);
+const burger = document.querySelector(".navbar-toggler-icon");
+console.log(burger);
 
-burger.addEventListener('click', function(){
-    body.classList.toggle('lock');
+burger.addEventListener("click", function () {
+  body.classList.toggle("lock");
 });
 
-
 function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-        change.target.classList.add('element-show');
-      }
-    });
-  }
-  let options = { threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
-
-
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+for (let elm of elements) {
+  observer.observe(elm);
+}
 
 const btnUp = {
-  el: document.querySelector('.btnUp'),
+  el: document.querySelector(".btnUp"),
   show() {
     // удалим у кнопки класс btn-up_hide
-    this.el.classList.remove('btn-up_hide');
+    this.el.classList.remove("btn-up_hide");
   },
   hide() {
     // добавим к кнопке класс btn-up_hide
-    this.el.classList.add('btn-up_hide');
+    this.el.classList.add("btn-up_hide");
   },
- 
-  
+
   addEventListener() {
     // при прокрутке содержимого страницы
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       // определяем величину прокрутки
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       // если страница прокручена больше чем на 300px, то делаем кнопку видимой, иначе скрываем
       scrollY > 300 ? this.show() : this.hide();
     });
     // при нажатии на кнопку .btn-up
-    document.querySelector('.btnUp').onclick = () => {
+    document.querySelector(".btnUp").onclick = () => {
       // переместим в начало страницы
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: 'smooth'
+        behavior: "smooth",
       });
-    }
-  }
-}
+    };
+  },
+};
 
 btnUp.addEventListener();
